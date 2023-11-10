c1d899320b2f88fccdbd619a4e1084b2078a7716
+dennisngangairungu <+>
2023-11-10 22:15:47 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React ,{useState , useEffect}from 'react'
+
+function App() {
+    const [one ,isOne] = useState([])
+    const [two, isTwo] = useState(false)
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(data => {
+        isOne(data)
+        isTwo(true)
+      })
+},[])
+console.log(one)
+
+if (!two) return <p>Loading...</p>
+
+  return (
+    <div>
+      <img src={one.message} alt='A Random Dog' />
+    </div>
+  )
+}
+
+export default App
+
